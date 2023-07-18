00de0394535282071733402d0567b452155b81db
Gleison Rocha <glucasmr@gmail.com>
2023-07-18 20:37:17 -0300

    feat: create User,TaskList and Task models and migrations

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function taskLists(){
+        return $this->hasMany(TaskList::class);//task_list_id 
+    }
 }
